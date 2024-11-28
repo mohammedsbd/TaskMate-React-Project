7786ab73e95ff9b916331d010b5c530d8a3b15de
mohammedsbd <Mohammed1112ok@gmail.com>
2024-11-28 09:56:03 -0800

    added an array

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,27 +1,34 @@
 import React from 'react'
 
 function ShowTask() {
+  const tasks = [
+    { id: 10001, name: "TASK A", time: "2:09:01 Am 9/14/2030" },
+    { id: 10002, name: "TASK B", time: "2:09:01 Am 9/14/2030" },
+    { id: 10003, name: "TASK C", time: "2:09:01 Am 9/14/2030" }
+  ];
   return (
-   <section className="showTask">
-   <div className="head">
-    <div> 
-      <span className='title'>Todo</span>
-      <span className='count'>0</span>
-    </div>
-    <button className='clearAll'>Clear All</button>
-   </div>
-   <ul>
-    <li>
-      <p>
-        <span className='name'>Task A </span>
-        <span className='time'>2:09:01 Am 9/14/2030</span>
-      </p>
-      <i className='bi bi-pencil-square'></i>
-      <i className='bi bi-trash'></i>
-    </li>
-   </ul>
-   </section>
-  )
+    <section className="showTask">
+      <div className="head">
+        <div>
+          <span className="title">Todo</span>
+          <span className="count">0</span>
+        </div>
+        <button className="clearAll">Clear All</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li>
+            <p>
+              <span className="name">{task.name} </span>
+              <span className="time">{task.time}</span>
+            </p>
+            <i className="bi bi-pencil-square"></i>
+            <i className="bi bi-trash"></i>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default ShowTask
